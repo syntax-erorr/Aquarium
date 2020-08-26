a6be01c6cc6b59699777e1726916ef0ddc6b3ef2
syntax-erorr <68997220+syntax-erorr@users.noreply.github.com>
2020-08-26 12:45:54 +1000

    Update aquarium.js
    fixed reset but still not sure

--- aquarium.js
+++ aquarium.js
@@ -1,25 +1,25 @@
 window.onload=function() {
-                lengthObj = document.getElementById('txtLength');
-                heightObj = document.getElementById('txtHeight');
-                widthtObj = document.getElementById('tdWidth');
-	  			costObj= document.getElementById("tdCost")
-                document.getElementById('btnReset').onclick = resetInputs;
-                document.getElementById('btnCalcCost').onclick = calcWeight;
-            }
-            function resetInputs() {
+	lengthObj = document.getElementById('txtLength');
+    heightObj = document.getElementById('txtHeight');
+    widthtObj = document.getElementById('txtWidth');
+	costObj= document.getElementById("tdCost");
+    document.getElementById('btnReset').onclick = resetInputs ();
+    document.getElementById('btnCalcCost').onclick = calcWeight();
+}
+function resetInputs() {
                 lengthObj.value = '';
                 widthObj.value = '';
                 heightObj.value = '';
 				costObj.innerHTML= '';
-            }
-            function calcWeight() {
-                var length = new Number(lengthObj.value);
-                var width = new Number(widthObj.value);
-				var height = new Number(heightObj.value);
-                costObj.innerHTML = '';
-                if(isNaN(length) || (isNaN(width)) || (isNaN(height) {
-                    alert('Invalid length or girth');
-                    return;
-                }
-                weightObj.innerHTML = length*Math.pow(girth,2)/800;
-            }
+}
+function calcWeight() {
+	var length = new Number(lengthObj.value);
+    var width = new Number(widthObj.value);
+	var height = new Number(heightObj.value);
+    costObj.innerHTML = '';
+    if(isNaN(length) || isNaN(width) || isNaN(height)) {
+		alert('Invalid length or girth');
+		return;
+    }
+    costObj.innerHTML = length*width
+}
